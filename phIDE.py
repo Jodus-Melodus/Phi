b433f5b8e7e2af139e7e3461fcb500076f04f8b7
Jodus-Melodus <philipgricius@gmail.com>
2023-12-02 08:39:02 +0200

    fixed icon #24

--- phIDE.py
+++ phIDE.py
@@ -94,6 +94,7 @@ class App(ctk.CTk):
         super().__init__()
         self.title('phIDE')
         self.state('zoomed')
+        self.iconbitmap('phi.ico')
         self.textBoxFont = ctk.CTkFont(family='Courier New', size=16, weight='bold')
         self.buttonFont = ctk.CTkFont(family='Fira Code', size=12, weight='normal')
         self.screenRatio = 0.65
